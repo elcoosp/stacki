d05bfa6f6669b8a5247c960d5e109acaa65b0faf
Elcoosp <elcoosp@gmail.com>
2018-06-03 19:39:45 +0200

    test(Stacki): Add test

---
+++ src/__tests__/Stacki.js
@@ -0,0 +1,18 @@
+const Stacki = require('../Stacki')
+let s
+beforeEach(() => {
+  s = Stacki()
+})
+
+test('should push items and return the stacki object', () => {
+  s.push(1, 2, 3).push(4)
+  expect(s.get()).toEqual([1, 2, 3, 4])
+})
+
+test('should pop items and return the stacki object', () => {
+  s
+    .push(1, 2, 3)
+    .pop()
+    .push(4)
+  expect(s.get()).toEqual([1, 2, 4])
+})
